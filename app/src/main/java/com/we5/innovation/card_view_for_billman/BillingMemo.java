3579254efaec7b2e99c2be009985fd048f983f98
emon109848 <emon109848>
2021-12-16 21:45:44 +0600

    Testing Version control

---
+++ app/src/main/java/com/we5/innovation/card_view_for_billman/BillingMemo.java
@@ -0,0 +1,14 @@
+package com.we5.innovation.card_view_for_billman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BillingMemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_billing_memo);
+    }
+}
---
+++ app/src/main/java/com/we5/innovation/card_view_for_billman/MainActivity.java
@@ -0,0 +1,16 @@
+package com.we5.innovation.card_view_for_billman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
